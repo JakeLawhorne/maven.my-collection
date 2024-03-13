4fd95b05589f547173ec8fb6d41e254a28a7ff8e
JakeLawhorne <jake.lawhorne@gmail.com>
2024-03-13 16:37:46 -0400

    finished Node class

--- src/main/java/com/github/curriculeon/MyNode.java
+++ src/main/java/com/github/curriculeon/MyNode.java
@@ -8,19 +8,27 @@ public class MyNode<SomeType> {
     }
 
     public MyNode(SomeType data, MyNode<SomeType> next) {
+            this.data = data;
+            this.next = next;
     }
 
     public SomeType getData() {
-        return null;
+        return this.data;
     }
 
     public void setData(SomeType data) {
+        this.data = data;
     }
 
     public MyNode<SomeType> getNext() {
-        return null;
+        return this.next;
     }
 
     public void setNext(MyNode<SomeType> next) {
+        if(this == next){
+            throw new IllegalArgumentException();
+        }else {
+            this.next = next;
+        }
     }
 }
